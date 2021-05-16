d71480ce5e5618cc189616ea4eb8dbd460cae1ff
Andrew Steinborn <git@steinborn.me>
2021-05-16 09:26:35 -0400

    Heh.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 val kotlinVersion = "1.5.0"
 
 plugins {
+    val kotlinVersion = "1.5.0"
+
     kotlin("jvm") version kotlinVersion
     kotlin("kapt") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
