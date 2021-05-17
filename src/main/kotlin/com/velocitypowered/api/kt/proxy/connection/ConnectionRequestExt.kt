6b032882e850035ab2fa7f7003c8066de779eb9f
Seppe Volkaerts <seppevolkaerts@hotmail.com>
2021-05-17 17:55:28 +0200

    Change to unix line ending.

--- src/main/kotlin/com/velocitypowered/api/kt/proxy/connection/ConnectionRequestExt.kt
+++ src/main/kotlin/com/velocitypowered/api/kt/proxy/connection/ConnectionRequestExt.kt
@@ -5,5 +5,5 @@ import com.velocitypowered.api.proxy.player.ConnectionRequestBuilder
 import com.velocitypowered.api.proxy.server.RegisteredServer
 import kotlinx.coroutines.future.await
 
-suspend fun Player.connect(server: RegisteredServer): ConnectionRequestBuilder.Result =
+suspend fun Player.connectTo(server: RegisteredServer): ConnectionRequestBuilder.Result =
   createConnectionRequest(server).connect().await()
